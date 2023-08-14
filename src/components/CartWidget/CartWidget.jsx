5f126d80e5d4e197d7eb076adc4670387d21729e
ElBorry <borriello1985@gmail.com>
2023-08-14 20:15:10 -0300

    "Actualizacion"

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -2,7 +2,7 @@ const CartWidget = () => {
   return (
     <div>
       <button className="btn btn-primary position-relative">
-        <i class="bi bi-cart-fill"></i>
+        <i className="bi bi-cart-fill"></i>
         <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
           1
           <span className="visually-hidden">Articulos Seleccionados</span>
